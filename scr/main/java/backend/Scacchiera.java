949218d8eeb1a77d3bdfd11fe59faef5eb7e73cc
fernandomorea <fernando8morea@gmail.com>
2022-08-06 10:28:48 +0200

    Scacchiera

--- scr/main/java/backend/Scacchiera.java
+++ scr/main/java/backend/Scacchiera.java
@@ -1,13 +1,13 @@
 package backend;
 
 public class Scacchiera {
-    static String[][] matrix = new String[8][8];
+    private String[][] matrix = new String[8][8];
 
-    public static void setPezzo(int x,int y,String pezzo){
-        matrix[x-1][y-1]=pezzo;
+    public void setPezzo(int x,int y,String pezzo){
+        this.matrix[x-1][y-1]=pezzo;
     }
 
-    public static void printMatrix(String[][] matrix){
+    public void print(){
         for (int x=7;x>=0;x--){
             for (int y=0;y<8;y++){
                 System.out.print(matrix[x][y]);
@@ -18,8 +18,9 @@ public class Scacchiera {
     }
 
     public static void main(String[] args) {
-        setPezzo(1,2,"pedina_bianca");
-        printMatrix(matrix);
+        Scacchiera scacchiera = new Scacchiera();
+        scacchiera.setPezzo(1,2,"pedina_bianca");
+        scacchiera.print();
     }
 
 }
