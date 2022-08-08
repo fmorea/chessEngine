ee308e97bce5faaa01932666b253f6b1832b519e
fernandomorea <fernando8morea@gmail.com>
2022-08-08 15:49:58 +0200

    En passant fix

--- scr/main/java/backend/Scacchiera.java
+++ scr/main/java/backend/Scacchiera.java
@@ -130,11 +130,14 @@ public class Scacchiera {
                     System.out.print(y+ " ");
                     y=y-1;
                 }
-                if (matrix[y][x]!=null){
+                if (matrix[y][x]!=null && matrix[y][x].charAt(0) != 'p'){
                     System.out.print(matrix[y][x]);
                 }
+                else if (matrix[y][x]!=null && matrix[y][x].charAt(0) == 'p'){
+                    System.out.print("ped" + getColorePezzo(y+1,x+1));
+                }
                 else{
-                    System.out.print(" __ ");
+                    System.out.print(" -- ");
                 }
                 if(x==7){
                     y=y+1;
@@ -168,12 +171,16 @@ public class Scacchiera {
             return false;
         }
         else{
-            String temp = this.getPezzo(y0,x0);
-            this.setPezzo(y0,x0,null);
-            this.setPezzo(y,x,temp);
+            forceMove(y0,x0,y,x);
             toccaAlBianco = !toccaAlBianco;
             return true;
         }
+    }
+
+    public void forceMove(int y0, int x0, int y, int x){
+        String temp = this.getPezzo(y0,x0);
+        this.setPezzo(y0,x0,null);
+        this.setPezzo(y,x,temp);
     }
 
     public char getTipoPezzo(int y,int x){
@@ -184,7 +191,7 @@ public class Scacchiera {
 
     }
     public char getColorePezzo(int y,int x){
-        if (this.getPezzo(y,x)!=null){
+        if (getPezzo(y,x)!=null){
             return this.getPezzo(y,x).charAt(3);
         }
         else return '0';
@@ -241,27 +248,75 @@ public class Scacchiera {
 
     public boolean legalPawnMove(int y0, int x0, int y, int x){
         boolean isLegalMove = false;
+        //Segnala che è la prima volta che muovo il pezzo (en passant related)
+        if (getPezzo(y0,x0).charAt(1)=='e'){
+            String pedone = getPezzo(y0,x0);
+            pedone = pedone.replace('e', '1');
+            setPezzo(y0,x0,pedone);
+        }
+        else{
+            String pedone = getPezzo(y0,x0);
+            pedone = pedone.replace('1', '_');
+            setPezzo(y0,x0,pedone);
+        }
         if (isNotRe(y,x)){
             isLegalMove = thereIsAnEatablePieceByPawn(y0,x0,y,x);
         }
         else if (isEmpty(y,x)){
             if (x0 == x){
                 isLegalMove = true;
+                // Segnala che il pedone si è mosso in avanti di uno (En passant related)
+                if (y == y0 + 1 || y == y0-1){
+                    String pedone = getPezzo(y0,x0);
+                    pedone = pedone.replace('d', '_');
+                    setPezzo(y0,x0,pedone);
+                }
             }
         }
         return  isLegalMove;
+    }
+
+    public boolean isEnPassantAble(int y, int x){
+        if(     getTipoPezzo(y,x) == 'p'     &&
+                getPezzo(y,x).charAt(2)=='d' &&
+                getPezzo(y,x).charAt(1)=='1'
+        ){
+            return true;
+        }
+        else return false;
     }
 
     private boolean isLegalMove(int y0, int x0, int y, int x) {
         boolean isLegalMove = false;
         switch(getTipoPezzo(y0,x0)) {
             case 'p': // pedina
+
+                // En passant fix
+                if (isEmpty(y,x)){
+                    if(isBianco(y+1,x) && y-1==4 && y0==4 &&
+                            getTipoPezzo(y+1,x)=='p' &&
+                            isEnPassantAble(y+1,x) &&
+                            getColorePezzo(y+1,x)!=getColorePezzo(y0,x0)
+                    ){
+                        forceMove(y+1,x,y,x);
+                    }
+                    if(isNero(y-1,x) && y-1==5 && y0==5 &&
+                            getTipoPezzo(y-1,x)=='p' &&
+                            isEnPassantAble(y-1,x) &&
+                            getColorePezzo(y-1,x)!=getColorePezzo(y0,x0)
+                    ){
+                        forceMove(y-1,x,y,x);
+                    }
+                }
+
+
+                // Standard pawn logic
                 if(isBianco(y0,x0)){
                     if((y0 == 2 && y == 4 ) || (y == y0 + 1 && isInsideChessBoard(y0+1))){
                         isLegalMove = legalPawnMove(y0,x0,y,x);
                         }
                     }
-                else if (isNero(y0,x0)){ // pedina nera
+                else if (isNero(y0,x0)){
                     if((y0 == 7 && y == 5 ) || (y == y0 - 1 && isInsideChessBoard(y0-1))){
                         isLegalMove = legalPawnMove(y0,x0,y,x);
                     }
